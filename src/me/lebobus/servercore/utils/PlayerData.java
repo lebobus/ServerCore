61735fb6d14d77ac1ffd751f5ca52ee4881a768b
lebobus <robertgaina@outlook.fr>
2019-04-11 02:02:10 -0400

    bugs fixed.

--- src/me/lebobus/servercore/utils/PlayerData.java
+++ src/me/lebobus/servercore/utils/PlayerData.java
@@ -1,14 +1,8 @@
 package me.lebobus.servercore.utils;
 
-import org.bukkit.BanEntry;
-import org.bukkit.BanList;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.BanList.Type;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerLoginEvent.Result;
 
@@ -23,10 +17,8 @@ public class PlayerData implements Listener {
 		
 		Player p = (Player) e.getPlayer();
 		
-		
 	    stats = new Files(Main.inst.getDataFolder(), "stats.yml");
 		stats.loadFile();
-
 		
 		if (e.getResult() == Result.KICK_BANNED) {
 			stats.set("player."+p.getUniqueId()+".name", null);
@@ -39,7 +31,6 @@ public class PlayerData implements Listener {
 			stats.saveFile();
 			return;
         }
-		
 		
 		if(stats.getString("player."+p.getUniqueId().toString()) == null) {
 	       stats.set("player."+p.getUniqueId()+".name", p.getName().toString());
